98dd74359b4b6e40d5842601a1ece79e68895fc9
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-11-06 01:37:12 +0800

    initial making the useStates and app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface ListItem {
+  id: string;
+  title: string;
 }
 
-export default App
+const getLocalStorage = (): ListItem[] => {
+  const list = localStorage.getItem('list');
+  if (list !== null) {
+    return JSON.parse(list) as ListItem[];
+  } else {
+    return [];
+  }
+};
+
+
+
+function App() {
+ 
+
+  const [name, setName] = useState<string>('');
+  const [list, setList] = useState<ListItem[]>(getLocalStorage());
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editID, setEditID] = useState<string | null>(null);
+ 
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          {/* <List items={list} removeItem={removeItem} editItem={editItem} /> still planning to make this component*/} 
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
+          <ToastContainer/>
+
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default App;
