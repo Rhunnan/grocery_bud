7840260e8418c806c0b3a4d72411da5ed6bcb083
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-11-06 01:38:18 +0800

    added the functions, for submit handler, remove, clearlist, edit Item

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,73 @@ function App() {
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [editID, setEditID] = useState<string | null>(null);
  
+  const notifyEmpty = () => toast.warning("Please Enter a Value");
+  const notifyAdded = () => toast.success("Successfuly Added");
+  const notifyEditSucc = () => toast.success("Successfuly Edited");
+  const notifyRemove = () => toast.warning("Successfuly Remove Item");
+
+
+
+
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!name) {
+      notifyEmpty();
+    } else if (name && isEditing) {
+      setList((prevList) =>
+        prevList.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      notifyEditSucc();
+    } else {
+      notifyAdded();
+      const newItem: ListItem = { id: new Date().getTime().toString(), title: name };
+      setList((prevList) => [...prevList, newItem]);
+      setName('');
+    }
+  };
+
+
+  const clearList = (): void => {
+    toast.success('Cleared All Items'); // Show a success toast
+ setList([]);
+  };
+
+  const removeItem = (id: string): void => {
+
+    notifyRemove();
+    setList((prevList) => prevList.filter((item) => item.id !== id));
+  };
+
+  
+
+
+  
+
+  const editItem = (id: string): void => {
+    const specificItem = list.find((item) => item.id === id);
+
+    if (specificItem) {
+      setIsEditing(true);
+      setEditID(id);
+      setName(specificItem.title);
+    }
+
+  };
+  
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
